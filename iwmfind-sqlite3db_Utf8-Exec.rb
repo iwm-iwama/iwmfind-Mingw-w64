c3c51f79519337b43f15f0089e5324f7d1bd4c57
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2021-08-25 16:47:21 +0900

    Add files via upload

--- iwmfind-sqlite3db_Utf8-Exec.rb
+++ iwmfind-sqlite3db_Utf8-Exec.rb
@@ -11,16 +11,20 @@
 #--------------------------------------------------------------------------------------------------
 
 $db = "#{Time.now.strftime("%Y%m%d_%H%M%S_%L")}.db"
+$iCnt = 0
 
 # (例)
 #   D:フォルダ以下、24時間以内に更新されたファイル名を抽出
 %x(iwmfind.exe "D:" -r -nh -nf -o="#{$db}" -w="type like 'f' and mtime>=[-24h]")
 
+print "                    \r"
+
 %x(sqlite3.exe "#{$db}" "select mtime||'\t'||path from V_INDEX order by mtime;").split("\n") do
 	|ln|
 	ln.strip!
 
-	puts "#{ln}"
+	$iCnt += 1
+	puts "#{$iCnt}\t#{ln}"
 	#
 	# 本処理をここに書く
 	# (例) File.delete ln
